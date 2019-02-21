df50aaad9abb4cd5bcc2bdc3994f9e37a39b7e47
Артем <artyom.cox@gmail.com>
2019-02-21 22:01:51 +0300

    added Test_1 template

---
+++ Ini Parser/src/tests/Test_1.cpp
@@ -0,0 +1,9 @@
+//
+//  Test_1.cpp
+//  Ini Parser
+//
+//  Created by Артем on 21/02/2019.
+//  Copyright © 2019 Artyom Panfutov. All rights reserved.
+//
+
+#include "Test_1.hpp"
---
+++ Ini Parser/src/tests/Test_1.hpp
@@ -0,0 +1,13 @@
+//
+//  Test_1.hpp
+//  Ini Parser
+//
+//  Created by Артем on 21/02/2019.
+//  Copyright © 2019 Artyom Panfutov. All rights reserved.
+//
+
+#ifndef Test_1_hpp
+#define Test_1_hpp
+
+
+#endif /* Test_1_hpp */
